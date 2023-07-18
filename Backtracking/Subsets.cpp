a5ba66cc2b5438e691cb5c960ad75759c3c6e3b8
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2023-07-18 11:47:01 +0500

    Create Subsets.cpp

---
+++ Backtracking/Subsets.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        set<vector<int>> res;
+        vector<int> temp;
+        recur(0,res,nums,temp);
+        return vector<vector<int>>(res.begin(),res.end());
+    }
+    void recur(int index, set<vector<int>>& res,vector<int>&nums,vector<int> current){
+        res.insert(current);
+        for(int i=index;i<nums.size();i++){
+            current.push_back(nums[i]);
+            recur(i+1,res,nums,current);
+            current.pop_back();
+        }
+    }
+};
