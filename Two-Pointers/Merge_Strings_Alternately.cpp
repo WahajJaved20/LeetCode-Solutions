43aa8ab85330a2c746f1a4c3b2955f80b1b05c39
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2024-01-08 20:48:56 +0500

    Update Merge_Strings_Alternately.cpp

--- Two-Pointers/Merge_Strings_Alternately.cpp
+++ Two-Pointers/Merge_Strings_Alternately.cpp
@@ -33,3 +33,34 @@ public:
         return result;
     }
 };
+
+
+// better approach
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int length1 = word1.length();
+        int length2 = word2.length();
+        if(!length1){
+            return word2;
+        }
+        if(!length2){
+            return word1;
+        }
+        int i=0, j=0;
+        string result = "";
+        while ( i<length1 || j<length2){
+            if(i<length1){
+                result += word1[i];
+                i++;
+            }
+            if(j<length2){
+                result += word2[j];
+                j++;
+            }
+                
+            
+        }
+        return result;
+    }
+};
