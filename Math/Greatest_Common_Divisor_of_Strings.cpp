a06f1de08ab6f048035ec0fdafae813f639fb5e6
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2024-01-08 21:00:12 +0500

    Create Greatest_Common_Divisor_of_Strings.cpp

---
+++ Math/Greatest_Common_Divisor_of_Strings.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        string result = "";
+        if(str1 + str2 != str2 + str1){
+            return result;
+        }
+        result =  str1.substr(0,gcd(str1.length(),str2.length()));
+        return result;
+    }
+};
